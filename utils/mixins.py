3511d5d6412e413751189bfb817d485f8ed23ba1
sameerarshad7771@gmail.com <sameerarshad7771@gmail.com>
2023-10-19 11:18:21 +0500

    feat: add mixins to project utils

---
+++ utils/mixins.py
@@ -0,0 +1,41 @@
+import uuid
+
+from datetime import timedelta
+
+from django.db import models
+from django.utils import timesince, timezone
+
+
+class UUID(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def created(self):
+        created = timesince.timesince(self.created_at).split(", ")[0]
+        x = timezone.now() - self.created_at
+        if int(x.total_seconds()) <= timedelta(seconds=10).seconds:
+            return "just now"
+        elif int(x.total_seconds()) <= timedelta(seconds=59).seconds:
+            return f"{int(x.total_seconds())} seconds ago"
+        return f"{created} ago"
+
+    def updated(self):
+        updated = timesince.timesince(self.updated_at).split(", ")[0]
+        x = timezone.now() - self.updated_at
+        if int(x.total_seconds()) <= timedelta(seconds=59).seconds:
+            return f"updated {int(x.total_seconds())} seconds ago"
+        return f"{updated} ago"
