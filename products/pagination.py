9a1b321c7a7ea757541fb572dabe0fa9c8493e37
sameerarshad7771@gmail.com <sameerarshad7771@gmail.com>
2023-11-29 19:18:42 +0500

    increase default page size limit

--- products/pagination.py
+++ products/pagination.py
@@ -8,6 +8,6 @@ class LargeResultsSetPagination(PageNumberPagination):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 40
     page_size_query_param = 'page_size'
     max_page_size = 100
